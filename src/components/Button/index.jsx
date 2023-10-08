0fe8b5e43bc250a064139ffffcbd5aa3e8b12921
baran ghanbari <shabghanbari44@gmail.com>
2023-10-08 16:02:51 +0330

    section2

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,19 @@
-import { red } from "@material-ui/core/colors";
-
 export default function index(props) {
-   
   return (
-    <button className=" h-10	rounded-md font-normal	text-sm	leading-5	text-center	"
-    style={{backgroundColor:props.bgcolor,padding:"0 15px",color:props.color,width:"135px",width:props.width,height:props.height,
-  fontSize:props.fontSize,marginRight:props.marginRight.backgroundColor,
-marginTop:props.marginTop}}>{props.value}</button>
-  )
+    <button
+      className=" h-10	rounded-md font-normal	text-sm	leading-5	text-center	"
+      style={{
+        backgroundColor: props.bgcolor,
+        padding: "0 15px",
+        color: props.color,
+        width: "135px",
+        width: props.width,
+        height: props.height,
+        fontSize: props.fontSize,
+        marginTop: props.marginTop,
+      }}
+    >
+      {props.value}
+    </button>
+  );
 }
